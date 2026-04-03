02c11bcc6406ada3d7cd911cda200e9d56063353
Ivan <ivan.lalov@hotmail.com>
2026-04-03 18:09:48 +0200

    Login funcional con Firebase Auth

--- app/src/main/java/com/lalov/frigory/MainActivity.kt
+++ app/src/main/java/com/lalov/frigory/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.lalov.frigory
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Inicializamos el motor de Firebase
+        mAuth = FirebaseAuth.getInstance()
+
+        // Referencias a los componentes del XML
+        val emailEdit = findViewById<EditText>(R.id.emailEditText)
+        val passwordEdit = findViewById<EditText>(R.id.passwordEditText)
+        val btnLogin = findViewById<Button>(R.id.loginButton)
+
+        btnLogin.setOnClickListener {
+            val email = emailEdit.text.toString()
+            val password = passwordEdit.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                // Intento de login en Firebase
+                mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "¡Bienvenido a Frigory!", Toast.LENGTH_SHORT).show()
+                            // Aquí es donde luego saltaremos a la pantalla de la nevera
+                        } else {
+                            Toast.makeText(this, "Error: ${task.exception?.message}", Toast.LENGTH_LONG).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this, "Rellena los campos", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
